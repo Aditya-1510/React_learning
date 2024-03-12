2c43d3a81fa73b52cada6c5d0027f01305d0d67d
AdityaPare <adityapare2001@gmail.com>
2024-03-12 19:53:54 +0530

    some new things

---
+++ 01basicsVitereact/vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import Chai from "./chai";
+function App() {
+  return (
+    <>
+      <Chai />
+      <h1>Chai or React | Aditya Pare </h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus error
+        quaerat dolorum eaque, iusto iste perferendis dolore facere incidunt vel
+        veniam. Iure, ut.
+      </p>
+    </>
+  );
+}
+
+export default App;
