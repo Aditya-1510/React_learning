718cea565d555f2c0895a955e2217a9e1e555d2f
AdityaPare <adityapare2001@gmail.com>
2024-03-13 22:07:19 +0530

    first section of training

---
+++ 01basicsreact/src/App.js
@@ -0,0 +1,14 @@
+import Chai from "./chai";
+function App() {
+  const username = "Chai aur code";
+  return (
+    <>
+      <Chai />
+
+      <h1>Chai or React {username} </h1>
+      <p>akjdfhlkasjhf lkjaflh lkafhiearygalkjsdfh iafhaufhlakszjdfn</p>
+    </>
+  );
+}
+
+export default App;
